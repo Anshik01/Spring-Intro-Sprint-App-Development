6be5cb95cc058e3830b16bb6a5bc6d7ee4d114ec
Anshik01 <anshik0706@gmail.com>
2025-02-22 12:33:54 +0530

    [Anshik Jaiswal] Add. using @RestController

--- src/main/java/com/uc/springlearning/hellocontroller/HelloController.java
+++ src/main/java/com/uc/springlearning/hellocontroller/HelloController.java
@@ -1,19 +1,11 @@
 package com.uc.springlearning.hellocontroller;
 
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class HelloController {
-    @GetMapping("/web")
+    @GetMapping("/hello")
     public String hello () {
-        return "Hello";
-    }
-    @GetMapping("/web/message")
-    public String message (Model model) {
-        model.addAttribute("message","This is a custom message");
-        return "message";
+        return "Hello from BridgeLabz";
     }
 }
