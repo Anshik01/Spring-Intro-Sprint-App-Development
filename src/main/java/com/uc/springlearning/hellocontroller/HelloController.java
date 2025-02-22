61769fc2aed4c09eea620e84841c71a62038b5a4
Anshik01 <anshik0706@gmail.com>
2025-02-22 12:19:52 +0530

    [Anshik Jaiswal] Add. render html

--- src/main/java/com/uc/springlearning/hellocontroller/HelloController.java
+++ src/main/java/com/uc/springlearning/hellocontroller/HelloController.java
@@ -1,15 +1,19 @@
 package com.uc.springlearning.hellocontroller;
 
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
-
-    @GetMapping(value = {"", "/"})
-    @ResponseBody
-    public String sayHello(){
-        return "Hello from BridgeLabz";
+    @GetMapping("/web")
+    public String hello () {
+        return "Hello";
+    }
+    @GetMapping("/web/message")
+    public String message (Model model) {
+        model.addAttribute("message","This is a custom message");
+        return "message";
     }
 }
